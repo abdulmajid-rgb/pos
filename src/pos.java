ae3a3b1c9c80a1c885ea2bb7ef08b42bc5e7af9c
Abdulmejid <your-github-email>
2020-06-16 22:34:45 -0400

    positive or negative

---
+++ src/pos.java
@@ -0,0 +1,11 @@
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+
+public class pos {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("TestData");
+        int TestData = input.nextInt();
+        System.out.println("35 is positive");
+        }
+}
